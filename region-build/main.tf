42eb4a0d8ab78d34dfdeef72a45c32f7105477e8
Ronald Lee <rlee@aviatrix.com>
2023-02-28 12:20:54 -0800

    Use latest revision of task defintion

--- region-build/main.tf
+++ region-build/main.tf
@@ -501,8 +501,9 @@ module "aviatrix_eventbridge" {
         attach_role_arn = true
 
         ecs_target = {
-          task_count          = 5
-          task_definition_arn = aws_ecs_task_definition.task_def.arn
+          task_count = 5
+          # Remove the revision number so that the latest revision of the task definition is invoked
+          task_definition_arn = trimsuffix(aws_ecs_task_definition.task_def.arn, ":${aws_ecs_task_definition.task_def.revision}")
           launch_type         = "FARGATE"
           network_configuration = {
             subnets          = var.use_existing_vpc ? var.subnet_names : tolist([aws_subnet.subnet[0].id, aws_subnet.subnet_ha[0].id])
