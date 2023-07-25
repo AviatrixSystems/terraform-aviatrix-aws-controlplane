9559d7ece96c907f38de0ddb2422652c1a043976
Ronald Lee <rlee@aviatrix.com>
2023-07-25 10:30:57 -0700

    Add default tags

--- aws.tf
+++ aws.tf
@@ -1,8 +1,18 @@
 provider "aws" {
   region = var.region
+  default_tags {
+    tags = {
+      Aviatrix-Created-Resource = "Do-Not-Delete-Aviatrix-Created-Resource"
+    }
+  }
 }
 
 provider "aws" {
   alias  = "region2"
   region = var.dr_region
+  default_tags {
+    tags = {
+      Aviatrix-Created-Resource = "Do-Not-Delete-Aviatrix-Created-Resource"
+    }
+  }
 }
