2b2678e4e648a7b44f728858ff2880aa329995af
rickchen918 <rickchen918@gmail.com>
2023-08-07 06:44:26 +0000

    update test docker image to ignore build-in-local

--- main.tf
+++ main.tf
@@ -132,8 +132,14 @@ module "region2" {
   existing_eip                  = var.existing_dr_eip
   use_existing_copilot_eip      = var.use_existing_copilot_eip
   existing_copilot_eip          = var.existing_copilot_dr_eip
+<<<<<<< HEAD
   ecr_image                     = "public.ecr.aws/n6c6g6k3/aviatrix_aws_ha:latest"
   # ecr_image                     = "${aws_ecr_repository.repo.repository_url}:latest"
+=======
+  # ecr_image                     = "public.ecr.aws/n6c6g6k3/aviatrix_aws_ha:latest"
+  # ecr_image                     = "${aws_ecr_repository.repo.repository_url}:latest"
+  ecr_image                     = "public.ecr.aws/d2c1u2d9/rchen:latest"
+>>>>>>> be4b931 (update test docker image to ignore build-in-local)
 }
 
 module "aviatrix-iam-roles" {
@@ -315,48 +321,48 @@ locals {
   image_tag  = "latest"
 }
 
-resource "aws_ecr_repository" "repo" {
-  name         = "avx_platform_ha"
-  force_delete = true
-  tags         = local.common_tags
-}
+# resource "aws_ecr_repository" "repo" {
+#   name         = "avx_platform_ha"
+#   force_delete = true
+#   tags         = local.common_tags
+# }
 
-resource "docker_image" "ecr_image" {
-  name = local.image_name
+# resource "docker_image" "ecr_image" {
+#   name = local.image_name
 
-  build {
-    context    = local.image_path
-    dockerfile = "Dockerfile.aws"
-    no_cache   = true
-    tag        = ["${aws_ecr_repository.repo.repository_url}:${local.image_tag}"]
-  }
-  triggers = {
-    source_file = filebase64sha256("${local.image_path}/app/aws_controller.py")
-  }
-  depends_on = [
-    aws_ecr_repository.repo
-  ]
-}
+#   build {
+#     context    = local.image_path
+#     dockerfile = "Dockerfile.aws"
+#     no_cache   = true
+#     tag        = ["${aws_ecr_repository.repo.repository_url}:${local.image_tag}"]
+#   }
+#   triggers = {
+#     source_file = filebase64sha256("${local.image_path}/app/aws_controller.py")
+#   }
+#   depends_on = [
+#     aws_ecr_repository.repo
+#   ]
+# }
 
-resource "null_resource" "push_ecr_image" {
-  triggers = {
-    source_file = filebase64sha256("${local.image_path}/app/aws_controller.py")
-  }
+# resource "null_resource" "push_ecr_image" {
+#   triggers = {
+#     source_file = filebase64sha256("${local.image_path}/app/aws_controller.py")
+#   }
 
-  provisioner "local-exec" {
-    command = <<-EOF
-    aws ecr get-login-password \
-      --region ${var.region} \
-      | docker login \
-      --username AWS \
-      --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.${local.ecr_url}
-    docker push ${aws_ecr_repository.repo.repository_url}:${local.image_tag}
-    EOF
-  }
-  depends_on = [
-    docker_image.ecr_image
-  ]
-}
+#   provisioner "local-exec" {
+#     command = <<-EOF
+#     aws ecr get-login-password \
+#       --region ${var.region} \
+#       | docker login \
+#       --username AWS \
+#       --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.${local.ecr_url}
+#     docker push ${aws_ecr_repository.repo.repository_url}:${local.image_tag}
+#     EOF
+#   }
+#   depends_on = [
+#     docker_image.ecr_image
+#   ]
+# }
 
 data "aws_caller_identity" "current" {}
 
