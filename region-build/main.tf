84aba2c61456b69e346d7326be4cbd7feadc1671
rickchen918 <rickchen918@gmail.com>
2023-08-08 01:21:24 +0000

    fix missing of iam type replacement

--- region-build/main.tf
+++ region-build/main.tf
@@ -367,7 +367,7 @@ data "aws_iam_policy_document" "ecs_task_execution_assume_role" {
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws-cn:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  policy_arn = "arn:${iam_type}:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 
