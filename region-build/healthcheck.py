dba8ae4a2d6eb9f9d393b7a1070bb357bb676a54
rlee-aviatrix <83723013+rlee-aviatrix@users.noreply.github.com>
2024-08-14 14:31:27 -0700

    fix: Add Controller login functions

--- region-build/healthcheck.py
+++ region-build/healthcheck.py
@@ -1,11 +1,18 @@
 import boto3
+import json
 import os
 import socket
 import sys
 import time
 import traceback
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
 from datetime import datetime
+from pip._vendor import requests
 from pprint import pprint
+
+
+urllib3.disable_warnings(InsecureRequestWarning)
 
 
 class AvxError(Exception):
@@ -73,6 +80,11 @@ def _lambda_handler(event, context):
     print("ssm_path:", ssm_path)
     print("ssm_region:", ssm_region)
     print("password:", password)
+
+    print("ip:", ip)
+    cid = login_to_controller(ip, "admin", password)
+    print("login_to_controller:", cid)
+    print("is_controller_ready", is_controller_ready_v2(ip, cid))
 
 
 def publish_message_to_sns(topic_arn, message, region):
@@ -164,3 +176,143 @@ def get_ssm_parameter_value(path, region):
     except Exception as e:
         print(e)
         sys.exit(1)
+
+
+mask = lambda input: input[0:5] + "*" * 15 if isinstance(input, str) else ""
+
+
+def get_api_token(ip_addr):
+    """Get API token from controller. Older controllers that don't support it will not have this
+    API or endpoints. We return None in that scenario to be backkward compatible"""
+    try:
+        data = requests.get(
+            f"https://{ip_addr}/v2/api?action=get_api_token", verify=False
+        )
+    except requests.exceptions.ConnectionError as err:
+        print("Can't connect to controller with IP %s. %s" % (ip_addr, str(err)))
+        raise AvxError(str(err)) from err
+    buf = data.content
+    if data.status_code not in [200, 404]:
+        err = f"Controller at {ip_addr} is not ready. Status code {data.status_code}  {buf}"
+        print(err)
+        raise AvxError(err)
+    try:
+        out = json.loads(buf)
+    except ValueError:
+        print(f"Token is probably not supported. Response is {buf}")
+        print("Did not obtain token")
+        return None
+    try:
+        api_return = out["return"]
+    except (KeyError, AttributeError, TypeError) as err:
+        print(
+            f"Getting return code failed due to {err}. Token may not be supported."
+            f"Response is {out}"
+        )
+        print("Did not obtain token")
+        return None
+    if api_return is False:
+        try:
+            reason = out["reason"]
+        except (KeyError, AttributeError, TypeError) as err:
+            print(f"Couldn't get reason. Response is {out}")
+            print("Did not obtain token")
+            return None
+        if reason == "RequestRefused":
+            err = f"Controller at {ip_addr} is not ready. Status code {reason} {out}"
+            print(err)
+            raise AvxError(err)
+        print(
+            f"Getting token failed due to {reason}. Token may not be supported."
+            f"Response is {out}"
+        )
+        print("Did not obtain token")
+        return None
+    try:
+        token = out["results"]["api_token"]
+    except (ValueError, AttributeError, TypeError, KeyError) as err:
+        print(f"Getting token failed due to {err}")
+        print(f"Token is probably not supported. Response is {out}")
+        print("Did not obtain token")
+        return None
+    print("Obtained token")
+    return token
+
+
+def login_to_controller(ip_addr, username, pwd):
+    """Logs into the controller and returns the cid"""
+    token = get_api_token(ip_addr)
+    headers = {}
+    base_url = "https://" + ip_addr + "/v1/api"
+    if token:
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "X-Access-Key": token,
+        }
+        base_url = "https://" + ip_addr + "/v2/api"
+    try:
+        response = requests.post(
+            base_url,
+            verify=False,
+            headers=headers,
+            data={"username": username, "password": pwd, "action": "login"},
+        )
+    except Exception as err:
+        print(
+            "Can't connect to controller with elastic IP %s. %s" % (ip_addr, str(err))
+        )
+        raise AvxError(str(err)) from err
+    try:
+        response_json = response.json()
+    except ValueError as err:
+        print(f"response not in json {response}")
+        raise AvxError("Unable to create session. {}".format(response)) from err
+    try:
+        cid = response_json.pop("CID")
+        print("Created new session with CID {}\n".format(mask(cid)))
+    except KeyError as err:
+        print(response_json)
+        print("Unable to create session. {} {}".format(err, response_json))
+        raise AvxError("Unable to create session. {}".format(err)) from err
+    print(response_json)
+    return cid
+
+
+def is_controller_ready_v2(
+    ip_addr="123.123.123.123",
+    CID="ABCD1234",
+):
+    start_time = time.time()
+    api_endpoint_url = "https://" + ip_addr + "/v2/api"
+    data = {"action": "is_controller_ready", "CID": CID}
+    print("API endpoint url:", str(api_endpoint_url))
+    payload_with_hidden_password = dict(data)
+    payload_with_hidden_password["CID"] = "************"
+    print(
+        f"Request payload: "
+        f"{str(json.dumps(obj=payload_with_hidden_password, indent=4))}"
+    )
+
+    while time.time() - start_time < 600:
+        try:
+            response = requests.get(
+                url=api_endpoint_url, params=data, verify=False, timeout=60
+            )
+            if response is not None:
+                py_dict = response.json()
+
+                if response.status_code == 200 and py_dict["return"] is True:
+                    print(f"Controller is ready to operate")
+                    return True
+                    break
+            else:
+                print(f"Controller is not ready")
+        except requests.Timeout:
+            print(f"The API request timed out after 60 seconds")
+        except Exception as err:
+            print(str(err))
+        print(f"Checking if controller is ready in 60 seconds.")
+        time.sleep(60)
+
+    print(f"Controller is not ready to operate")
+    return False
