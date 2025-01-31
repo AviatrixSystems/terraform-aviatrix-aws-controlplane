400e6b317832b74f21665294f2cfb330c887b925
rlee-aviatrix <83723013+rlee-aviatrix@users.noreply.github.com>
2025-01-31 08:42:15 -0800

    fix: Create a new backup after successful restore

--- docker/app/inter_region_v2.py
+++ docker/app/inter_region_v2.py
@@ -163,6 +163,20 @@ def health_check_handler(msg_json):
         if response_json["return"] == True:
             failover = "completed"
 
+        ## Create a new backup so that filename uses new_private_ip
+        if response_json.get("return", False) is True:
+            print("Successfully restored backup")
+
+            print("Pause before setting up new backup")
+            # Sleep to avoid [AVXERR-MAINTENANCE-004] Upgrade/Restore/Migration in progress message
+            time.sleep(60)
+
+            # If restore succeeded, update private IP to that of the new instance now.
+            print("Creating new backup")
+            aws_controller.setup_ctrl_backup(
+                local_priv_ip, cid, local_env["PRIMARY_ACC_NAME"], "true"
+            )
+
         # 5. Migrate IP
 
         if s3_ctrl_version and int(s3_ctrl_version.split(".")[0]) >= 7:
